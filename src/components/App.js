0400d9a2a13d3a162a17545dbf67edaf9ea3cc02
Viktor Gofman <vsgof@yandex.ru>
2023-05-23 20:00:31 +0300

    feat: блок intro

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
-import logo from '../images/logo.svg';
 import './styles/App.css';
 import Header from './Header';
+import Intro from './Intro';
 
 function App() {
   return (
     <div className="App">
       <div className='page'>
         <Header />
+        <Intro />
       </div>
     </div>
   );
---
+++ src/components/Intro.js
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './styles/Intro.css';
+
+function Intro() {
+  return (
+    <div className='intro'>
+      <h1 className='intro__title'>The Trail Runner SWT Is Here.</h1>
+      <p className='intro__subtitle'>Durable. Comfortable. Ready to get dirty.</p>
+      <div className='intro__buttons'>
+        <button className='intro__button'>Shop Men</button>
+        <button className='intro__button'>Shop Women</button>
+      </div>
+    </div>
+  )
+}
+
+export default Intro;
